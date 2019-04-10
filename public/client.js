397c1d06f8f4b5173b78cb883e65498e5cc6773a
Marco Da Silva <marcodsman@gmail.com>
2019-04-10 17:38:51 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,15 +1,37 @@
 $( document ).ready(function() {
   
+  /* global io */
+  var socket = io();
   
-   
   // Form submittion with new message in field with id 'm'
   $('form').submit(function(){
     var messageToSend = $('#m').val();
+    socket.emit('chat message', messageToSend);
     //send message to server here?
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
   
+  
+  socket.on('user', function(data){
+    
+    $('#num-users').text(data.currentUsers + ' users online');
+    var message = data.name;
+    if(data.connected){
+      message += ' has joined the chat.';
+    } else {
+      message += ' has left the chat.';
+    }
+    $('#messages').append($('<li>').html('<b>' + message + '</b>'));
+  });
+  
+  socket.on('chat message', function(data){
+    console.log(data);
+    $('#messages').append($('<li>').html(data.name + ': ' + data.message) );
+  })
+  
+  
+
   
   
 });
